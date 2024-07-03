639efc89a1f6b4afb964368d35b7583459fa7376
Antwi Ebenezer <antwiebenezer824@gmail.com>
2024-07-03 04:39:26 +0000

    feat: create GlobalStyle for consistent application-wide styling
    - Added a GlobalStyle component to define application-wide CSS styles.
    - Applied global styles for typography, margins, paddings, and default element styles.
    - Ensured GlobalStyle is included at the root level of the application.
    
    This addition promotes consistent styling across the application and simplifies the management of global CSS rules.

---
+++ src/globalStyles.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+// Create a global style component using styled-components
+const GlobalStyle = createGlobalStyle`
+  // Define a custom font-face
+  @font-face {
+    font-family: 'JetBrainsMono';
+    src: url('/fonts/static/JetBrainsMono-Bold.ttf') format('truetype');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  // Apply global styles to the body element
+  body {
+    margin: 0; 
+    padding: 0; 
+    box-sizing: border-box; 
+    background-color: #08070B;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100svh; 
+    font-family: 'JetBrainsMono', sans-serif;
+  }
+`;
+
+// Export the global style component
+export default GlobalStyle;
